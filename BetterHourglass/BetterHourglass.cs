643a9b613be7bb52b9f2e84e3132bef6cd6c1688
TheCodeYT <eryk.cz2@outlook.com>
2022-05-26 10:41:12 +0200

    initial commit

---
+++ BetterHourglass/BetterHourglass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using UnityEngine;
+
+namespace BetterHourglass
+{
+    [BepInPlugin(pluginGuid, pluginName, pluginVersion)]
+    public class BetterHourglass
+    {
+        public const string pluginGuid = "betterhourglass";
+        public const string pluginName = "Better Hourglass";
+        public const string pluginVersion = "0.1.0";
+
+        public void Awake()
+        {
+
+        }
+    }
+}
